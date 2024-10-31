29175c0a0a511eca098786363929d2de5f45be0a
maximwlg <106670738+maximwlg@users.noreply.github.com>
2024-10-31 22:00:54 +0300

    Add files via upload

---
+++ 5/1.py
@@ -0,0 +1,7 @@
+# -- coding: utf-8 --
+n = int(input('Введите N: '))
+def main(n):
+    for i in range(n):
+        if i**2 <= n:
+            print(i**2)
+main(n)
---
+++ 5/2.py
@@ -0,0 +1,17 @@
+# -- coding: utf-8 --
+def input():
+    n = 0
+    while n < 2:
+        n = int(input('Введите N, не меньшее 2: '))
+        if n >= 2:
+            break
+        else:
+            print('Число должно быть не меньше 2')
+    return n
+
+def main():
+    n = input()
+    for i in range(2, n + 1):
+        if n % i == 0:
+            return i
+print(main())
